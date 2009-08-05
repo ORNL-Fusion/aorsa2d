3a19d402ff529751071dd77dcf4afa6a091d89ee
David Green <dlg0@me.com>
2009-08-05 14:54:41 -0400

    changed psimask to psilim in the creation of the mask

--- src/aorsa2dMain.F90
+++ src/aorsa2dMain.F90
@@ -3600,7 +3600,7 @@ end do
          do j = 1, nnodey
 
             mask(i,j) = 1
-            if (psi(i,j) .gt. psimask) mask(i,j) = 0
+            if (psi(i,j) .gt. psilim) mask(i,j) = 0
 
             if (limiter_boundary) then
                 eqdsk_box_mask: &
