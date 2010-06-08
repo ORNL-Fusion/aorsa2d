1bd0835346960ba451c8d097f9d30cb53efd9495
dlg0 <dlg0@me.com>
2010-06-08 17:02:04 -0400

    Added grid block initialization function. Not used but is there in preperation.

--- src/grid.f90
+++ src/grid.f90
@@ -9,9 +9,13 @@ implicit none
 
 type :: gridBlock
 
-    integer :: nX, nY
-    real, allocatable, dimension(:) :: xNorm, yNorm, R, z 
-    real :: rMin, rMax, zMin, zMax
+    integer :: nR, nZ
+    real, allocatable, dimension(:) :: rNorm, zNorm, R, z, kPhi
+    real :: rMin, rMax, zMin, zMax, rRange, zRange
+    real :: normFacR, normFacZ
+    integer :: nMin, nMax, mMin, mMax
+    real :: k_cutOff
+    complex, allocatable, dimension(:,:) :: xx, yy    
 
 end type gridBlock
 
@@ -30,6 +34,182 @@ integer :: nMin, nMax, mMin, mMax
 complex, allocatable, dimension(:,:) :: xx, yy
 
 contains
+
+    function init_gridBlock ( nR, nZ, rMin, rMax, zMin, zMax ) result ( grid )
+
+        use aorsa2din_mod, &
+        only : nPhi, xkPerp_cutOff
+        use parallel
+
+        implicit none
+
+        type(gridBlock) :: grid
+
+        integer, intent(in) :: nR, nZ
+        real, intent(in) :: rMin, rMax, zMin, zMax
+
+        integer :: i, j, n, m
+
+        allocate ( grid%rNorm(nR), &
+                    grid%zNorm(nZ), &
+                    grid%R(nR), &
+                    grid%z(nZ), &
+                    grid%kPhi(nR) )
+
+            ! Populate grid block parameters
+            ! ------------------------------
+
+            grid%rRange  = rMax - rMin    
+
+            grid%rMin = rMin
+            grid%rMax = rMax
+            grid%zMin = zMin
+            grid%zMax = zMax
+            grid%nR = nR
+            grid%nZ = nZ
+
+            
+            ! Create the grid block grids
+            ! ---------------------------
+
+            if(nR>1) then 
+
+                do i = 1, nR
+     
+                    if(chebyshevX) then
+                        ! Gauss-Lobatto grid [-1,1] (Chebyshev basis)
+                        grid%rNorm(i)  = -cos(pi*(i-1)/(nR-1))
+                    else
+                        ! Uniform grid [0,2pi] (Fourier basis)
+                        grid%rNorm(i)  = (i-1) * 2 * pi / ( nR - 1 )
+                    endif
+       
+                enddo
+            else
+
+                grid%R(1) = rMin
+                grid%rNorm(1) = 0
+                
+            endif
+
+            if(nR>1) &
+            grid%R = (grid%rNorm-grid%rNorm(1)) &
+                / (grid%rNorm(nR)-grid%rNorm(1)) * grid%rRange + rMin
+
+
+            if(chebyshevX)then
+                grid%normFacR = 2 / grid%rRange
+            else
+                grid%normFacR = 2 * pi / grid%rRange
+            endif
+
+       
+            grid%zRange  = zMax - zMin
+            if(nZ>1) then 
+
+                do j = 1, nZ
+
+                    if(chebyshevY) then
+                        ! Gauss-Lobatto grid [-1,1] (Chebyshev basis)
+                        grid%zNorm(j)  = -cos(pi*(j-1)/(nZ-1))
+                    else
+                        ! Uniform grid [0,2pi] (Fourier basis)
+                        grid%zNorm(j)  = (j-1) * 2 * pi / ( nZ - 1 )
+                    endif
+
+                enddo
+            else
+                grid%z(1)    = 0
+                grid%zNorm(1) = 0
+            endif
+
+            if(nZ>1) &
+            grid%z = (grid%zNorm-grid%zNorm(1)) &
+                / (grid%zNorm(nZ)-grid%zNorm(1)) * grid%zRange + zMin
+
+            if(chebyshevY) then
+                grid%normFacZ = 2 / grid%zRange
+            else 
+                grid%normFacZ = 2 * pi / grid%zRange
+            endif
+
+
+            ! Set the grid block kPhi
+            ! -----------------------
+
+            grid%kPhi = nPhi / grid%R
+
+
+            ! Set the grid block n,m ranges
+            ! -----------------------------
+
+            if(chebyshevX) then
+
+                grid%nMin    = 0
+                grid%nMax    = nR-1
+
+            else
+
+                grid%nMin = -nR/2
+                grid%nMax =  nR/2
+
+                ! Catch for even number of modes
+                ! for producing a square matrix
+                ! ------------------------------
+
+                if (mod(nR,2)==0) grid%nMin = grid%nMin+1
+
+            endif
+
+
+            if(chebyshevY) then
+
+                grid%mMin    = 0
+                grid%mMax    = nZ-1
+
+            else
+
+                grid%mMin = -nZ/2
+                grid%mMax =  nZ/2
+                if (mod(nZ,2)==0) grid%mMin = grid%mMin+1
+
+            endif
+
+            if (iAm==0) then
+                write(*,*) '    n: ', grid%nMin, grid%nMax
+                write(*,*) '    m: ', grid%mMin, grid%mMax
+            endif
+
+
+            ! Set the k above which will be damped
+            ! ------------------------------------
+
+            grid%k_cutOff = xkPerp_cutOff * sqrt(&
+               (grid%nMax * grid%normFacR)**2+(grid%mMax*grid%normFacZ)**2)
+
+
+            ! Fill the grid block basis functions
+            ! -----------------------------------
+
+            allocate ( &
+                grid%xx(grid%nMin:nMax,nR), &
+                grid%yy(grid%mMin:mMax,nZ) )
+
+            do i = 1, nR
+                do n = grid%nMin, grid%nMax 
+                    grid%xx(n,i) = xBasis(n,grid%rNorm(i))
+                enddo
+            enddo
+
+            do j = 1, nZ
+                do m = grid%mMin, grid%mMax 
+                    grid%yy(m,j) = yBasis(m,grid%zNorm(j))
+                enddo
+            enddo
+
+
+    end function init_gridBlock
+
 
     subroutine init_grid ()
 
